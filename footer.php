5dc85ed82a2187785c33756b0e7825583b99a388
Dmitry <Dmitry@L11-AudioLab.local>
2016-11-28 22:24:32 -0800

    styling home page

--- footer.php
+++ footer.php
@@ -11,17 +11,55 @@
 
 ?>
 
+<div class="contact-section-bg">
+    <div class="contact-section">
+        <div class="contact-info">  
+            <span>CALL US 111-1111-1111</span><button class="contact-btn">Contact Us</button>
+        </div>   
+
+        <div class="quote">
+        “Lorem ipsum dolor sit amet, et libero volutpat diam justo lorem. Mauris dis ac massa. Suspendisse purus, lacus non faucibus"
+        <br>
+        - Mauris dis 
+        </div>
+
+    </div>
+</div>
+
 	</div><!-- #content -->
+    <div class="footer-bg">
+    <!-- 
+        <footer id="colophon" class="site-footer" role="contentinfo">
+            <div class="site-info"></div>
+        </footer> #colophon 
+    -->
+        
+<footer class="footer" role="contentinfo">
+  <div class="footer-links">
+    <ul>
+      <li><h3>Content</h3></li>
+      <li><a href="javascript:void(0)">About</a></li>
+      <li><a href="javascript:void(0)">Contact</a></li>
+      <li><a href="javascript:void(0)">Products</a></li>
+    </ul>
+    <ul>
+      <li><h3>Follow Us</h3></li>
+      <li><a href="javascript:void(0)">Facebook</a></li>
+      <li><a href="javascript:void(0)">Twitter</a></li>
+      <li><a href="javascript:void(0)">YouTube</a></li>
+    </ul>
+    <ul>
+      <li><h3>Legal</h3></li>
+      <li><a href="javascript:void(0)">Terms and Conditions</a></li>
+      <li><a href="javascript:void(0)">Privacy Policy</a></li>
+    </ul>
+  </div>
 
-<p>footer</p>
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'cet' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'cet' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'cet' ), 'cet', '<a href="http://www.cetransportation.com/" rel="designer">Dmitry</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+</footer>
+
+        
+    </div>
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
--- header.php
+++ header.php
@@ -23,27 +23,67 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'cet' ); ?></a>
 
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
+        <div class="header-bg">
+            <header id="masthead" class="site-header" role="banner">
+                <div class="site-branding">
+                    <?php
+                    if ( is_front_page() && is_home() ) : ?>
+                        <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+                    <?php else : ?>
+                        <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><img src="http://src.wordpress-develop.dev/wp-content/uploads/2016/11/cet-logo.png"></a></p>
+                    <?php
+                    endif;
 
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
+                    $description = get_bloginfo( 'description', 'display' );
+                    if ( $description || is_customize_preview() ) : ?>
+                        <p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+                    <?php
+                    endif; ?>
+                </div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'cet' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+                <nav id="site-navigation" class="main-navigation" role="navigation">
+                    <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'cet' ); ?></button>
+                    <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+                </nav><!-- #site-navigation -->
+                
+                <div class="banner">
+                    
+                    <?php if ( is_front_page() ) : ?>
+                    
+                        <h2 class="home-h">Motivating Exellence</h2>
+                    
+                    <?php else : ?>
+                    
+                        <h2 class="overview-h"><?php echo the_title() ?></h2>
+                    
+                    <?php endif; ?>
+                    
+                </div>
+            </header><!-- #masthead -->
+        </div>
+    
+    <?php if ( is_front_page() ) : ?>
+    
+        <div class="triangle-container">
+            <div class="triangle-clip"></div>
+            <h2>We’re a full-service transportation engineering firm</h2>
+            
+            <p>Our primary service areas include traffic engineering, transportation planning, and electrical engineering. Our goal is to provide clients with creative and innovative solutions to build safe, efficient, and sustainable transportation systems while more efficiently utilizing the existing infrastructure. </p>
+            <br>
+            <p><a>Read more about Concord</a></p>
+        </div>
+    
+     <?php endif; ?>        
+        
 
 	<div id="content" class="site-content">
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
